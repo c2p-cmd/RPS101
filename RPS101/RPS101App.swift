0ce465b5d870754a7be06e875b488fe58c76eff1
Sharan Thakur <sharandt19@live.com>
2023-12-06 13:04:58 +0530

    Initial Commit

---
+++ RPS101/RPS101App.swift
@@ -0,0 +1,17 @@
+//
+//  RPS101App.swift
+//  RPS101
+//
+//  Created by Sharan Thakur on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RPS101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
